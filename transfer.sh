ebdde4ffdbd1b6c75965bd3cbb5db8a2aae15f03
xun-neon <gongxuncd@gmail.com>
2020-04-24 17:35:13 +0800

    quick

--- transfer.sh
+++ transfer.sh
@@ -5,7 +5,3 @@ prefix=$1 # acmdb-lab1
 tgt_dir=../submitdb/$prefix/src/java/simpledb/
 src_dir=../db/$prefix/src/main/java/simpledb/
 cp ${src_dir}/* ${tgt_dir}
-
-tgt_dir=../submitdb/$prefix/test/simpledb/
-src_dir=../db/$prefix/src/test/java/simpledb/
-cp -r ${src_dir}/* ${tgt_dir}
